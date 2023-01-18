0e4e6e11a822e94c94c17353150dda6f92a51233
oTuff <oscartuff@protonmail.com>
2023-01-18 13:15:14 +0100

    finished us2

--- src/pages/UserPanel.jsx
+++ src/pages/UserPanel.jsx
@@ -3,8 +3,10 @@ import rentalFacade from "../utils/rentalFacade.js";
 import userFacade from "../utils/userFacade.js";
 
 function UserPanel() {
-    const [tenantId, setTenantId] = useState()
+    const [tenantId, setTenantId] = useState();
     const [rentals, setRentals] = useState([]);
+    // const [selectedHouse, setSelectedHouse] = useState(0);
+    const [showHouse, setShowHouse] = useState(false);
     const [refresh, setRefresh] = useState(false);
 
     // useEffect(() => {
@@ -17,8 +19,19 @@ function UserPanel() {
     // }, [refresh]);
 
     const getRentals = (event) => {
-        rentalFacade.getRentalsByTenant(tenantId).then((data)=>{setRentals(data)})
+        rentalFacade.getRentalsByTenant(tenantId).then((data) => {
+            setRentals(data)
+        })
         event.preventDefault();
+    }
+
+    const house = (data) => {
+        return (
+            <div>
+                <td>{data.houseHouse.houseNumberOfRooms}</td>
+                <td>{data.houseHouse.address.streetAddress}, {data.houseHouse.address.cityInfo.zipCode} {data.houseHouse.address.cityInfo.cityName}</td>
+            </div>
+        )
     }
 
     const handleChange = (event) => {
@@ -34,10 +47,13 @@ function UserPanel() {
     return (
         <div className="tableBody">
             <h1>Rentals</h1>
-           <h1><form onSubmit={getRentals}>
-                <td>get rentals by tenant id:<input type="number" placeholder={"tenant id"} onChange={handleChange} name={"title"} maxLength={45}/></td>
-                <button onClick={getRentals}>find rentals</button>
-           </form></h1>
+            <h1>
+                <form onSubmit={getRentals}>
+                    <td>get rentals by tenant id:<input type="number" placeholder={"tenant id"} onChange={handleChange}
+                                                        name={"title"} maxLength={45}/></td>
+                    <button onClick={getRentals}>find rentals</button>
+                </form>
+            </h1>
             <table>
                 <thead>
                 <tr className={"blue"}>
@@ -46,8 +62,13 @@ function UserPanel() {
                     <th>annual price</th>
                     <th>deposit</th>
                     <th>contact person</th>
-                    <th>number of room</th>
-                    <th>address</th>
+                    <th>house info</th>
+                      {showHouse ? (
+                                <>
+                                    <th>number of room</th>
+                                    <th>address</th>
+                                </>
+                            ) : (<div></div>)}
                 </tr>
                 </thead>
                 <tbody>
@@ -59,8 +80,25 @@ function UserPanel() {
                             <td>{data.rentalPriceAnnual}</td>
                             <td>{data.rentalDeposit}</td>
                             <td>{data.rentalContactPerson}</td>
-                            <td>{data.houseHouse.houseNumberOfRooms}</td>
-                            <td>{data.houseHouse.address.streetAddress}, {data.houseHouse.address.cityInfo.zipCode} {data.houseHouse.address.cityInfo.cityName}</td>
+                            <td>
+                                <button onClick={() => {
+                                    setShowHouse(!showHouse)
+                                    // if(selectedHouse ===0) {
+                                        setSelectedHouse(data.id)
+                                    // }else{
+                                    //     setSelectedHouse(0)
+                                    // }
+
+                                } }>toggle house details</button>
+                            </td>
+                            {showHouse ? (
+                                <>
+                                    <td>{data.houseHouse.houseNumberOfRooms}</td>
+                                    <td>{data.houseHouse.address.streetAddress}, {data.houseHouse.address.cityInfo.zipCode} {data.houseHouse.address.cityInfo.cityName}</td>
+                                </>
+                            ) : (<></>)}
+                            {/*<td>{data.houseHouse.houseNumberOfRooms}</td>*/}
+                            {/*<td>{data.houseHouse.address.streetAddress}, {data.houseHouse.address.cityInfo.zipCode} {data.houseHouse.address.cityInfo.cityName}</td>*/}
                         </tr>
                     )
                 })}
